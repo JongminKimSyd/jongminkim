d57ec986c85f9a33221b3385872f83d4425d382d
Jongmin Kim <Jongmin.Kim@mynrma.com.au>
2022-09-10 09:12:47 +1000

    Update resume, layout

--- src/component/resume/resume.js
+++ src/component/resume/resume.js
@@ -12,7 +12,7 @@ class WorkHistoryRow extends React.Component {
     render() {
         const historyItem = this.props.historyItem;
         let endDate = historyItem.endDate === "" ? "Present" : historyItem.endDate;
-        
+
         return (
             <div className="row item">
                 <div className="twelve columns">
@@ -36,31 +36,13 @@ export default class Resume extends React.Component {
         const workHistory = this.props.workhistory;
         const workHistoryRows = [];
 
-        workHistory.forEach((workhistoryItem)=> {
+        workHistory.forEach((workhistoryItem) => {
             workHistoryRows.push(<WorkHistoryRow historyItem={workhistoryItem} key={workhistoryItem.name} />);
         });
 
 
         return (
             <section id="resume">
-                {/* Education----------------------------------------------- */}
-                <div className="row education">
-                    <div className="three columns header-col">
-                        <h1><span>Education</span></h1>
-                    </div>
-                    <div className="nine columns main-col">
-                        <div className="row item">
-                            <div className="twelve columns">
-                                <h3>University of Canberra</h3>
-                                <p className="info">Bachelor of Information Technology <span>•</span> <em className="date">Dec 2013</em></p>
-                                <p>This course has provided me extensive experience in developing information technology to address the needs of modern organisations, Teamwork, project management and communication skills, 
-                                Exploration of the technical and human aspects of information technology and its use, Extensive practical experience using Visual Basic .Net, C++ and C# within the Microsoft .Net Framework,
-                                Analysis and specification methods include UML, XML, structured and soft systems methodologies.
-                                </p>
-                            </div>
-                        </div> {/* item end */}
-                    </div> {/* main-col end */}
-                </div> {/* End Education */}
                 {/* Work----------------------------------------------- */}
                 <div className="row work">
                     <div className="three columns header-col">
@@ -84,6 +66,24 @@ export default class Resume extends React.Component {
                         </div>{/* end skill-bars */}
                     </div> {/* main-col end */}
                 </div> {/* End skills */}
+                {/* Education----------------------------------------------- */}
+                <div className="row education">
+                    <div className="three columns header-col">
+                        <h1><span>Education</span></h1>
+                    </div>
+                    <div className="nine columns main-col">
+                        <div className="row item">
+                            <div className="twelve columns">
+                                <h3>University of Canberra</h3>
+                                <p className="info">Bachelor of Information Technology <span>•</span> <em className="date">Dec 2013</em></p>
+                                <p>This course has provided me extensive experience in developing information technology to address the needs of modern organisations, Teamwork, project management and communication skills,
+                                    Exploration of the technical and human aspects of information technology and its use, Extensive practical experience using Visual Basic .Net, C++ and C# within the Microsoft .Net Framework,
+                                    Analysis and specification methods include UML, XML, structured and soft systems methodologies.
+                                </p>
+                            </div>
+                        </div> {/* item end */}
+                    </div> {/* main-col end */}
+                </div> {/* End Education */}
             </section>
 
         );
